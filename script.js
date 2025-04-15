ba8d7fe12662c6e1556eff5d80d04e6f0dab5273
AatifM <119080567+AatifM@users.noreply.github.com>
2025-04-15 17:30:25 -0400

    Finishing touches
    - Added a end of game declaration.
    - Cleaned up if statements for cleaner code.

--- script.js
+++ script.js
@@ -1,78 +1,52 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
 
-    if (choice === 1) {
-        return "rock";
-    }
-
-    else if (choice === 2) {
-        return "paper";
-    }
-
-    else {
-        return "scissors";
-    }
+    if (choice === 1) {return "rock";}
+    else if (choice === 2) {return "paper";}
+    else {return "scissors";}
 }
 
 function getHumanChoice() {
-    let choice = prompt("Enter rock, paper, or scissors: ");
-    return choice.toLowerCase();
+    return prompt("Enter rock, paper, or scissors: ").toLowerCase();
 }
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+    let humanSelection;
+    let computerSelection;
 
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === "scissors") {
             if (computerChoice === "paper") {
                 console.log(`You win! ${humanChoice} beats ${computerChoice}`);
                 humanScore += 1;
-            }
-            else if (computerChoice === "rock") {
+            } else if (computerChoice === "rock") {
                 console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
                 computerScore += 1;
-            }
-            else {
-                console.log("Tie!");
-            }
+            } else {console.log("Tie!");}
         }
 
         else if (humanChoice === "paper") {
             if (computerChoice === "rock") {
                 console.log(`You win! ${humanChoice} beats ${computerChoice}`);
                 humanScore += 1;
-            }
-
-            else if (computerChoice === "scissors") {
+            } else if (computerChoice === "scissors") {
                 console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
                 computerScore += 1;
-            }
-
-            else {
-                console.log("Tie!");
-            }
+            } else {console.log("Tie!");}
         }
 
         else {
             if (computerChoice === "scissors") {
                 console.log(`You win! ${humanChoice} beats ${computerChoice}`);
                 humanScore += 1;
-            }
-
-            else if (computerChoice === "paper") {
+            } else if (computerChoice === "paper") {
                 console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
                 computerScore += 1;
-            }
-
-            else {
-                console.log("Tie!");
-            }
+            } else {console.log("Tie!");}
         }
     }
-
-    let humanSelection;
-    let computerSelection;
 
     for (i = 1; i <= 5; i++) {
         humanSelection = getHumanChoice();
@@ -80,6 +54,16 @@ function playGame() {
 
         playRound(humanSelection, computerSelection);
     }
+
+    declareWinner(humanScore, computerScore);
+}
+
+function declareWinner(humanScore, computerScore) {
+    if (humanScore > computerScore) {
+        console.log(`You won! The score was ${humanScore}\-${computerScore}`);
+    } else if (computerScore > humanScore) {
+        console.log(`You lost :( The score was ${humanScore}\-${computerScore}`);
+    } else {console.log(`Tied! The score was ${humanScore}\-${computerScore}`);}
 }
 
 playGame();
